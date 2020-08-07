81585c2aaa890a50acd7c4481af92290200c2a03
Aarni Koskela <akx@iki.fi>
2020-08-07 10:58:26 +0300

    Make CSV/XML cases more equal

--- read_xml_to_pd.py
+++ read_xml_to_pd.py
@@ -1,4 +1,5 @@
 import xml.sax
+
 import pandas as pd
 
 from util import start_memory_usage_thread
@@ -13,12 +14,27 @@ class BadgeHandler(xml.sax.ContentHandler):
             self.rows.append(dict(attributes.items()))
 
 
-t, stop_event = start_memory_usage_thread()
-try:
+def read_rows():
     rows = []
     handler = BadgeHandler(rows)
     xml.sax.parse("badges.xml", handler)
     print("Finished XML")
+    return rows
+
+
+def make_df(rows):
+    print("Making DF...")
     df = pd.DataFrame(rows)
+    print("Cleaning up rows...")
+    rows[:] = []
+    print("Coercing date...")
+    df["Date"] = pd.to_datetime(df["Date"])
+    print("Completing...")
+    return df
+
+
+t, stop_event = start_memory_usage_thread()
+try:
+    df = make_df(read_rows())
 finally:
     stop_event.set()
